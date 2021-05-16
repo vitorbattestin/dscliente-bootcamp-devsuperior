47053d8f2e693dd7e38c03ccd95daadda8d4f8d9
Vitor Battestin <vitor.battestin@gmail.com>
2021-05-16 15:23:10 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
+++ backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
@@ -16,7 +16,10 @@ public class Client implements Serializable {
     private String name;
     private String cpf;
     private Double income;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant birthDate;
+
     private Integer children;
 
     public Client(){
--- backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -4,6 +4,7 @@ import com.devsuperior.dsclient.entities.Client;
 import com.devsuperior.dsclient.respositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class ClientService {
     @Autowired
     public ClientRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Client> findAll(){
         return repository.findAll();
     }
